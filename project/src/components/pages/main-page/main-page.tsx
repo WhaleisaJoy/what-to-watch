2f99ae694a2a021f2d9b8b5c0cd1fb10b507df23
Maria <afterlifelineonomega@gmail.com>
2022-09-04 23:25:13 +0300

    Refactoring main page

--- project/src/components/pages/main-page/main-page.tsx
+++ project/src/components/pages/main-page/main-page.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import type { Film } from '../../../types/film';
 import { ActiceCard } from '../../../types/types';
 import FilmCardList from '../../film-card-list/film-card-list';
+import Footer from '../../footer/footer';
 import Genres from '../../genres/genres';
+import Logo from '../../logo/logo';
+import UserBlock from '../../user-block/user-block';
 
 type MainPageProps = {
   filmCardsCount: number;
@@ -28,26 +30,8 @@ function MainPage({filmCardsCount, promoFilm, films}: MainPageProps): JSX.Elemen
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <ul className="user-block">
-            <li className="user-block__item">
-              <div className="user-block__avatar">
-                <Link to="/mylist" title="/mylist">
-                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-                </Link>
-              </div>
-            </li>
-            <li className="user-block__item">
-              <a className="user-block__link">Sign out</a>
-            </li>
-          </ul>
+          <Logo />
+          <UserBlock />
         </header>
 
         <div className="film-card__wrap">
@@ -99,19 +83,7 @@ function MainPage({filmCardsCount, promoFilm, films}: MainPageProps): JSX.Elemen
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   );
